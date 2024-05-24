f745ca324ecb5f85bd1938b2ddc737d3ce6d8004
keorgtz <kevin12ortizgt@gmail.com>
2024-05-24 07:21:51 -0600

    more changes

--- reactist/src/index.jsx
+++ reactist/src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {Coffe} from './components/coffe';
+import {Usuarios} from './components/Usuarios';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Coffe />
+    <Usuarios />
   </React.StrictMode>
 );
 
