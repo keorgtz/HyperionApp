e6bd24ff11ccf38a16fa8e19e3c7a4821d1282f8
keorgtz <kevin12ortizgt@gmail.com>
2024-05-26 13:26:12 -0600

    Movie App / API Init

---
+++ 1-peliculas/src/data/httpClient.js
@@ -0,0 +1,10 @@
+const API = "https://api.themoviedb.org/";
+export function get(path) {
+  return fetch(API + path, {
+    headers: {
+      Authorization:
+        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMWVkNThiZGNjZWVhZTQ3ZWRjMGJkMDE0NTU0MWI4NiIsInN1YiI6IjY2NTM4OGM0ZGZkYWM1NDgxODBhZWU5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xrQ5_HqGE7K8C43YIUhdmCKWAAaP8qcmnXmaG62y-eY",
+      "Content-Type": "application/json;charset=utf-8"
+    }
+  }).then((result) => result.json());
+}
