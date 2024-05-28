99d24f6a14411399ac1b02a0b71393899aa4168a
keorgtz <kevin12ortizgt@gmail.com>
2024-05-28 15:08:57 -0600

    Catalogo, css y key IDs
    se desplego ya el catalogo de peliculas, se añadieron los CSS de las tarjetas y el contenedor y vinculamos las ID de la API a cada pelicula.

--- 1-peliculas/src/components/MovieCard.jsx
+++ 1-peliculas/src/components/MovieCard.jsx
@@ -1,5 +1,17 @@
+
+import "../components/MovieCard.css"
 export function MovieCard({movie}) {
 const ImageUrl ="https://image.tmdb.org/t/p/w500" + movie.poster_path;
 
-  return <h1>Movie Card</h1>;
+
+  return (
+    <li className="movieCard">
+      <img width={230} 
+      height={345} 
+      src={ImageUrl}
+      alt={movie.title} 
+      className="movieImage" />
+      <div>{movie.title}</div>
+    </li>
+  )
 }
