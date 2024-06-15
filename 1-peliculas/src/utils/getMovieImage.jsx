797082d1a2f5ad1554a2ff950eb5c207cc6793f8
keorgtz <kevin12ortizgt@gmail.com>
2024-06-15 09:27:16 -0600

    Only ReactTS Project
    Delete all projects on React Only stay ReactTS

--- 1-peliculas/src/utils/getMovieImage.jsx
+++
@@ -1,7 +0,0 @@
-import placeholder from "../img/placeholder.png"
-
-export function getMovieImage(path,width){
-    return path? `https://image.tmdb.org/t/p/w${width}/${path}`:placeholder
-}
-// const ImageUrl ="https://image.tmdb.org/t/p/w500" + movie.poster_path;
-
